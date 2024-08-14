394ac5a3a6683c80223b2b534280fa39f376a714
Jiho Kim <jiho3634@gmail.com>
2024-08-14 09:35:31 +0900

    cler

--- src/main/java/com/beyond/ordersystem/member/dto/MemberRefreshDto.java
+++
@@ -1,8 +0,0 @@
-package com.beyond.ordersystem.member.dto;
-
-import lombok.*;
-
-@Getter
-public class MemberRefreshDto {
-    private String refreshToken;
-}
--- src/main/java/com/beyond/ordersystem/ordering/domain/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package com.beyond.ordersystem.ordering.domain;
-
-public enum OrderStatus {
-    ORDERED, CANCELED
-}
